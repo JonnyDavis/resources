ff7ec0c899454840879b5f9970eee8e30ba13541
Jonny Davis <Jonny@Jonnys-MacBook-Pro.local>
2017-11-01 12:39:41 +0000

    additions to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,27 @@ let sass = require('gulp-sass');
 let watch = require('gulp-watch');
 let gulpSequence = require('gulp-sequence');
 
+gulp.task('sass', function () {
+    var stream = gulp.src('./scss/styles.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css/'))
+        .pipe(rename('styles.css'));
+    return stream;
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('css/styles.css')
+	.pipe(cleanCSS({compatibility: 'ie8'}))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('./css/'));
+});
+
+
+gulp.task('styles', function(callback){ 
+	gulpSequence('sass', 'minify-css')(callback)
+});
+
+
+gulp.task('watch', function () {
+	gulp.watch('./scss/*.scss', ['styles']);
+});
