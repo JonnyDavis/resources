747d4d7501fa1cf8a058f11e09154395ff094beb
Jonny Davis <Jonny@Jonnys-MacBook-Pro.local>
2017-11-01 12:32:51 +0000

    adding resources files

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+let rename = require('gulp-rename');
+let sass = require('gulp-sass');
+let watch = require('gulp-watch');
+let gulpSequence = require('gulp-sequence');
+
